bf30db5b32cece5e6cb3e1c16e133fa35c08aa61
Cameron Hunter <chunter@twitter.com>
2016-04-14 14:12:23 -0700

    Build separate parsers optimized for speed and size as well as minified versions

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,26 @@
-import babel from "rollup-plugin-babel";
-import pegjs from "rollup-plugin-pegjs";
+import babel from 'rollup-plugin-babel';
+import pegjs from 'rollup-plugin-pegjs';
+import uglify from 'rollup-plugin-uglify';
+
+const nonNull = (array) => array.filter(x => !!x);
+
+const optimize = process.env.PEGJS_OPTIMIZE || 'speed';
+const isProduction = process.env.NODE_ENV === 'production';
+
+const dest = nonNull(['build/index', optimize, isProduction && 'min', 'js']).join('.');
 
 export default {
-  entry: "src/grammar.pegjs",
-  dest: "dist/index.js",
-  format: "cjs",
-  plugins: [
-    pegjs(),
-    babel({ babelrc: false, presets: ['es2015-rollup', 'stage-1'] })
-  ]
+  entry: 'src/grammar.pegjs',
+  dest,
+  format: 'cjs',
+  plugins: nonNull([
+    pegjs({
+      optimize
+    }),
+    babel({
+      babelrc: false,
+      presets: ['es2015-rollup', 'stage-1']
+    }),
+    isProduction && uglify()
+  ])
 }
